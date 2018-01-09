76a8bc83b102a4f1ce4c4a833f703353c61ac7f0
Sopan <sopan.emilence@gmail.com>
2018-01-09 17:54:12 +0530

    Add post

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var todos = [];
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Meet Mom for lunch',
-	completed: false
-},{
-	id: 2,
-	description: 'Go to market',
-	completed: false
-},{
-	id: 3,
-	description: 'Drink water',
-	completed: true
-}];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res){
 	res.send('Todo API Root');
 });
-
+/************************************************************************************************************************************
+*																																	*
+*													Get All Todos Items																*
+*																																	*
+************************************************************************************************************************************/
 // GET /todos
 app.get('/todos', function (req, res){
 	res.json(todos);
 })
 
-
+/************************************************************************************************************************************
+*																																	*
+*													Get Todos Items by Id															*
+*																																	*
+************************************************************************************************************************************/
 app.get('/todos/:id', function (req, res){
 	var todoId = req.params.id;
 	var matchedTodo;
@@ -41,11 +41,26 @@ app.get('/todos/:id', function (req, res){
 	else{
 		res.status(404).send();
 	}
-
-	
 	//res.send('Asking for todo with if of '+ req.params.id);
 });
 
-app.listen(PORT, function () {
+/************************************************************************************************************************************
+*																																	*
+*													POST Todos Items																*
+*																																	*
+************************************************************************************************************************************/
+app.post('/todos', function (req, res) {
+	var body = req.body;
+
+	body.id = todoNextId;
+	todoNextId++;
+
+	todos.push(body);
+
+	res.json(body);
+})
+
+var server = app.listen(PORT, function () {
 	console.log('Express listening on port '+ PORT+ '!');
 });
+server.timeout = 2500;
